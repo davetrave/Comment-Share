1341307d45bc2744f979bcc08a79f99543416b1a
davetrave <daveatmwh@gmail.com>
2026-01-02 08:35:45 +0300

    Fixed localhost for testing

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,4 +1,5 @@
-const baseUrl = process.env.REACT_APP_BASE_URL;
+// const baseUrl = process.env.REACT_APP_BASE_URL;
+const baseUrl = "http://localhost:8000/api";
 
 const appConfig = {
   apiPrefix: baseUrl,
